80414ffd2025644e38c196d2e5f9ca83ed67acad
ybx <15868861416@163.com>
2019-09-01 22:30:14 +0800

    this update for cutting a excel sheet

--- com/infervision/deal_excel/segmention_excel.py
+++ com/infervision/deal_excel/segmention_excel.py
@@ -6,14 +6,71 @@ Author: ybx
 import pandas as pd
 import datetime
 import re
-src_path = "/media/tx-eva-data/NAS/模板_new/tmp/标注/标注记录/数据标注记录_正式标注_分割_V1.1.xlsx"
+import xlrd
+import xlwt
+import os
+
+
 
 # from xlrd import open_workbook
 
-sheet = pd.read_excel(src_path,sheet_name=[0,1])
+# print(len(x1.sheet_names))
+# for i in range(7):
+#     print(i)
+# pd.read_excel(src_path, sheet_name=len(x1.sheet_names))
+
+# df_raw=pd.read_excel(content,engine='xlrd')
+
 # print(sheet[0]['日期'].to_list())
-# date = sheet[0]['日期'].to_list()
-print(sheet[0].loc[0:1])
+# date = list(sheet[0]['日期'].values)
+# print(list(sheet[0].loc[1].values))
+# for index, day in enumerate(date):
+#     print(str(index), day)
+# # print(date)
+# print(sheet[0].iloc[['2017.6.9'], :])
+# print(type(sheet[0].columns))
+# print(sheet[0].loc[['2017.6.9',]])
+# count = 0
+# new_list = []
+# for index, day in enumerate(date):
+#     if re.match('^\d\.*', str(day)):
+#         if day not in new_list:
+#             count +=1
+#             new_list.append(day)
+#             df = pd.DataFrame(columns=sheet[0].columns)
+#             df.loc[0] = list(sheet[0].loc[index].values)
+#             df.to_csv(save_path+'/'+str(day) + '.xls',mode='a', index=False, encoding='utf-8')
+#             count =0
+#         else:
+#             df1 = pd.read_csv(save_path+'/'+str(day) + '.xls',encoding='utf-8')
+#             df1.loc[count+1] = list(sheet[0].loc[index].values)
+#             df1.to_csv(save_path+'/'+str(day) + '.xls', mode='a', index=False,encoding='utf-8')
+
+#
+#
+# print("去重之前的长度:%s" % count)
+# print("去重之后的长度是:%s" % len(new_list))
+
+# df = pd.DataFrame(columns=sheet[0].columns)
+# for k,v in enumerate(sheet[0]['日期'].values):
+#     t = 0
+#     if str(v) in new_list:
+#         if t >= 1:
+#             df1 = pd.DataFrame()
+#             df1.loc[t-1] = sheet[0].loc[k]
+#             pd.concat([df, df1], ignore_index=True)
+#             t += 1
+#             df.to_csv(save_path+'/'+str(v) + '.xls',mode='a', index=False, encoding='gbk')
+#         df.loc[t] = sheet[0].loc[k]
+#         t +=1
+#         df.to_csv(save_path+'/'+str(v) + '.xls',mode='a', index=False, encoding='gbk')
+
+
+
+
+
+
+
 
 # tmp = 0
 # try:
@@ -35,3 +92,38 @@ print(sheet[0].loc[0:1])
 
 
 # def deal_date(src_path):
+
+
+
+def get_every_date_excel(src_path, save_path):
+    content = xlrd.open_workbook(filename=src_path, encoding_override='gbk')
+    x1 = pd.ExcelFile(src_path)
+    sheets = pd.read_excel(content, engine='xlrd', sheet_name=None, header=1)
+    for key in sheets:
+        date = list(sheets[key]['日期'].values)
+        # print(list(date))
+        count = 0
+        new_list = []
+        for index, day in enumerate(date):
+            if re.match('^\d\.*', str(day)):
+                if day not in new_list:
+                    count +=1
+                    new_list.append(day)
+                    df = pd.DataFrame(columns=sheets[key].columns)
+                    df.loc[0] = list(sheets[key].loc[index].values)
+                    df.to_csv(save_path+'/'+str(day) + '.xls',mode='a', index=False, encoding='utf-8')
+                    count =0
+                else:
+                    df1 = pd.read_csv(save_path+'/'+str(day) + '.xls',encoding='utf-8')
+                    df1.loc[count+1] = list(sheets[key].loc[index].values)
+                    df1.to_csv(save_path+'/'+str(day) + '.xls', mode='a', index=False,encoding='utf-8')
+
+
+if __name__ == "__main__":
+    src_path = "d:/aa/a/数据标注记录_正式标注_分割_V1.1.xlsx"
+    # save_path = '/'.join(src_path.split('/')[:-1]) +'/'+src_path.split('/')[-1].split('_')[2]
+    save_path = "d:/aa/a/segment"
+    # if not os.path.exists(save_path):
+    #     os.makedirs(save_path)
+
+    get_every_date_excel(src_path, save_path)
--- com/infervision/ruku/first.md
+++ com/infervision/ruku/first.md
@@ -5,7 +5,19 @@
 ## 关于ubuntu下面的zip压缩文件解码乱码的问题
     unzip -O CP936 xx.zip
 
-##    
+## 关于pandas里面的read_csv函数的说明
+    read_csv函数里面的参数是
+    pandas.read_csv(filepath_or_buffer, sep=', ', delimiter=None, header='infer', names=None, index_col=None, usecols=None, squeeze=False, prefix=None, mangle_dupe_cols=True, dtype=None, engine=None, converters=None, true_values=None, false_values=None, skipinitialspace=False, skiprows=None, nrows=None, na_values=None, keep_default_na=True, na_filter=True, verbose=False, skip_blank_lines=True, parse_dates=False, infer_datetime_format=False, keep_date_col=False, date_parser=None, dayfirst=False, iterator=False, chunksize=None, compression='infer', thousands=None, decimal=b'.', lineterminator=None, quotechar='"', quoting=0, escapechar=None, comment=None, encoding=None, dialect=None, tupleize_cols=None, error_bad_lines=True, warn_bad_lines=True, skipfooter=0, doublequote=True, delim_whitespace=False, low_memory=True, memory_map=False, float_precision=None）
+    
+    sep=','   # 以，为数据分隔符
+    shkiprows= 10   # 跳过前十行
+    nrows = 10   # 只去前10行
+    parse_dates = ['col_name']   # 指定某行读取为日期格式
+    index_col = ['col_1','col_2']   # 读取指定的几列
+    error_bad_lines = False   # 当某行数据有问题时，不报错，直接跳过，处理脏数据时使用
+    na_values = 'NULL'   # 将NULL识别为空值
+    header 默认是0 行作为列索引
+    
    
    
    
